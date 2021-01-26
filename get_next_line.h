1ba4aa63d597baf92b6ba224a0e9e8f5e2a2c615
TaeWungYou <y7k7380@naver.com>
2021-01-26 20:15:56 +0900

    2

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyou <tyou@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 20:40:14 by tyou              #+#    #+#             */
+/*   Updated: 2021/01/26 16:15:19 by tyou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100000
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+int		get_next_line(int fd, char **line);
+#endif
